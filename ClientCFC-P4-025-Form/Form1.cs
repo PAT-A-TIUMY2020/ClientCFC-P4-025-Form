a206f39c05dd84ed0534c832c313209882aae133
Riyantama <62780613+ryntm23@users.noreply.github.com>
2020-10-30 07:14:51 +0700

    Add project files.

---
+++ ClientCFC-P4-025-Form/Form1.cs
@@ -0,0 +1,76 @@
+﻿using ServiceMtk_P1_025;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientCFC_P4_025_Form
+{
+    public partial class Form1 : Form
+    {
+        BasicHttpBinding bind = new BasicHttpBinding();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ChannelFactory<IMatematika> objChannel = new ChannelFactory<IMatematika>(bind, "http://localhost:1907");
+            IMatematika obj = objChannel.CreateChannel();
+
+            int a, b, c;
+            a = Convert.ToInt32(textBox3.Text);
+            b = Convert.ToInt32(textBox4.Text);
+            c = a - b;
+            textBox10.Text = c.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ChannelFactory<IMatematika> objChannel = new ChannelFactory<IMatematika>(bind, "http://localhost:1907");
+            IMatematika obj = objChannel.CreateChannel();
+
+            int a, b, c;
+            a = Convert.ToInt32(textBox5.Text);
+            b = Convert.ToInt32(textBox6.Text);
+            c = a * b;
+            textBox11.Text = c.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ChannelFactory<IMatematika> objChannel = new ChannelFactory<IMatematika>(bind, "http://localhost:1907");
+            IMatematika obj = objChannel.CreateChannel();
+
+            int a, b, c;
+            a = Convert.ToInt32(textBox7.Text);
+            b = Convert.ToInt32(textBox8.Text);
+            c = a / b;
+            textBox12.Text = c.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ChannelFactory<IMatematika> objChannel = new ChannelFactory<IMatematika>(bind, "http://localhost:1907");
+            IMatematika obj = objChannel.CreateChannel();
+
+            int a, b, c;
+            a = Convert.ToInt32(textBox1.Text);
+            b = Convert.ToInt32(textBox2.Text);
+            c = a + b;
+            textBox9.Text = c.ToString();
+        }
+    }
+}
---
+++ ClientCFC-P4-025-Form/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceMtk_P1_025;using System.Windows.Forms;
+
+namespace ClientCFC_P4_025_Form
+{
+    class Program
+    {
+     
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+  
+    }
+}
